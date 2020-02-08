2555467359924cdb18e4d9565184938aca884f40
Hubert Chen <hubert94536@yahoo.com>
2020-02-08 12:38:37 -0800

    splitting into functions

--- dymano_commands.py
+++ dymano_commands.py
@@ -20,49 +20,55 @@ dynamodb = boto3.resource('dynamodb')
 dynamoTable = dynamodb.Table('test_table1')
 
 # PutItem
-response = dynamoTable.put_item(
-    Item={
-        'name': "test0",
-        'age': 35,
-        'id': decimal.Decimal(144244)
-    }
-)
-
+def putItem(str name, int age, int id) {
+    response = dynamoTable.put_item(
+        Item={
+            'name': name,
+            'age': age,
+            'id': decimal.Decimal(id)
+        }
+    )
+}
 # print("PutItem succeeded:")
 # print(json.dumps(response, indent=4, cls=DecimalEncoder))
 
 # Get Item
-get_name = "test1"
 
-try:
+def getItem(str name) {
+    get_name = name
+
+    try:
     response = dynamoTable.get_item(
         Key={
             'name': get_name
         }
     )
-except ClientError as e:
-    print(e.response['Error']['Message'])
-else:
-    item = response['Item']
-    print("Data :")
-    print(json.dumps(item, indent=4, cls=DecimalEncoder))
-
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        item = response['Item']
+        print("Data :")
+        print(json.dumps(item, indent=4, cls=DecimalEncoder))
+}
 # Update Item
-insert_name = "test1"
-try :
-    response = dynamoTable.update_item(
-     Key={
-        'name': insert_name,
-     },
-     UpdateExpression="set age=:a, id=:i",
-     ConditionExpression='attribute_exists(id)',
-     ExpressionAttributeValues={
-        ':i': decimal.Decimal(1111111),
-        ':a': 50
-     },
-     ReturnValues="UPDATED_NEW"
-    )
-except ClientError as e:
-    print(e.response['Error']['Message'])
 
-print("Update Item succeeded")
+def updateItem(str name, int id, int age) {
+    insert_name = name
+    try :
+        response = dynamoTable.update_item(
+        Key={
+            'name': insert_name,
+        },
+        UpdateExpression="set age=:a, id=:i",
+        ConditionExpression='attribute_exists(id)',
+        ExpressionAttributeValues={
+            ':i': decimal.Decimal(id),
+            ':a': age
+        },
+        ReturnValues="UPDATED_NEW"
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+
+    print("Update Item succeeded")
+}
